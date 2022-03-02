2fbe945a5b560864d5bd872d96e7313780201a0e
Uduak Essien <acushla4real@gmail.com>
2022-03-02 13:42:53 +0100

    Implement login and dashboard functions

--- components/ui/navbar/index.js
+++ components/ui/navbar/index.js
@@ -1,19 +1,34 @@
 
 import { ActiveLink, Button } from "@components/ui";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 export default function Navbar() {
-  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [userState, setUserState] = useState(false);
 
+  useEffect(() => {
+    async function fetchUser() {
+      const res = await fetch("/api/auth/me");
+      setUserState(res.ok)
+    }
+
+    fetchUser();
+
+  }, []);
+
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  
+ 
    const MENU = [
      { title: "Beat Makers", url: "/#" },
      { title: "Beat Users", url: "/#" },
      { title: "Pricing", url: "/#" },
      { title: "Licenses", url: "/#" },
      { title: "About Us", url: "/#" },
-     { title: "Contact", url: "/#" },
+     { title: "Contact", url: "/#" }
    ]
+
 
     return (
     <nav className="border-gray-200 px-2 sm:px-4 py-2.5 bg-gray-900 opacity-80">
@@ -30,7 +45,7 @@ export default function Navbar() {
         <button
          onClick={() => setNavbarOpen(!navbarOpen)}
          data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
+          <span className="sr-only">Open main menu {console.log("user state", userState)}</span>
           <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
           <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
         </button>
@@ -44,6 +59,11 @@ export default function Navbar() {
               </ActiveLink>
             </li>
             ))}
+            <li>
+              <ActiveLink href={`/${userState ? 'dashboard' : '/api/auth/login'}`}>
+              <a className="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{`${userState ? 'Dashboard' : 'Login'}`}</a>
+              </ActiveLink>
+            </li>
           </ul>
         </div>
 
